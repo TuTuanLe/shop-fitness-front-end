11624a178039b0e07166f243e1de18ff6222bb79
TuTuanLe <tutuanletkpt@gmail.com>
2022-08-15 22:45:38 +0700

    fix:tsx

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React, {ReactElement} from "react";
+import { Container } from "./styles";
+
+const HomePage = (): ReactElement => {
+    return(
+        <Container>
+            <h1>hello</h1>
+        </Container>
+    )
+}
+
+export default HomePage;
--- src/screens/auth/login.controller.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ComponentType } from "react";
-
-export const LoginController = <P,>(Component : ComponentType<P>,) : ComponentType<P> =>{
-    return (props : P) =>{
-        return <Component {...props} />
-    }
-}
