ad52a0169f8c827908cd404b0fb04475556041ad
TuTuanLe <tutuanletkpt@gmail.com>
2022-08-16 22:31:06 +0700

    fix: styled componenent

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React, {ReactElement} from "react";
-import { Container } from "./styles";
+// import { Container } from "./styles";
+import styled from "styled-components";
 
 const HomePage = (): ReactElement => {
     return(
@@ -10,3 +11,20 @@ const HomePage = (): ReactElement => {
 }
 
 export default HomePage;
+
+
+export const Container = styled.div`
+`;
+
+
+export const Logo = styled.img`
+    width: 6em;
+    border:1px solid #131A22;
+    padding: .2em .1em;
+    cursor:pointer;
+
+    &:hover{
+        border:1px solid #ffffff;
+        border-radius: .2em;
+    }
+`
